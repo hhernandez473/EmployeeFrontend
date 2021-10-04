917c4a78da3eb83b5d3181267580f27e4c37dd6a
unknown <hernandez.hb473@gmail.com>
2021-10-04 01:44:05 -0600

    Frontend add missing methods

---
+++ src/app/core/models/Employee.ts
@@ -0,0 +1,18 @@
+export interface Employee {
+    id:              number;
+    name:            string;
+    lastname:        string;
+    genderId:        number;
+    maritalStatusId: number;
+    birthdayDate:    Date;
+    age:             number;
+    dpi:             string;
+    nit:             string;
+    socialSecurity:  string;
+    irtra:           string;
+    passport:        string;
+    address:         string;
+    jobPositonID:    number;
+    salary:          number;
+    bonus:           number;
+}
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -5,6 +5,7 @@ import { Gender } from '../core/models/Gender';
 import { ApiService } from '../core/service/api.service';
 import { MaritalStatus } from '../core/models/MaritalStatus';
 import Swal from 'sweetalert2'
+import { Employee } from '../core/models/Employee';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -16,8 +17,10 @@ export class EmployeeComponent implements OnInit {
   genderList: Gender[] = [];
   maritalStatusList: MaritalStatus[] = [];
   EmployeeList: any [] = [];
-  columnsEmployed: string[]= ['name', 'lastname', 'dpi', 'total'];
+  columnsEmployed: string[]= ['name', 'lastname', 'dpi', 'total', 'edit', 'del'];
   jobPositionList: any[] = [];
+  btnEdit: boolean = false;
+  employeeId : number = 0;
   constructor(
     private api: ApiService,
     private fb: FormBuilder
@@ -32,23 +35,23 @@ export class EmployeeComponent implements OnInit {
     this.getJobPosition();
   }
 
-  createFormGroup() {
+  createFormGroup(data?: Employee) {
     return this.fb.group({
-      name: ["", [Validators.required]],
-      lastname: ["", [Validators.required]],
-      genderId: [1,[Validators.required]],
-      maritalStatusId: [1,[Validators.required]],
-      birthdayDate: [new Date(),[Validators.required]],
-      age: [0,[Validators.required]],
-      dpi: [0,[Validators.required]],
-      nit: [0],
-      socialSecurity: [0],
-      irtra: [0],
-      passport: [0,[Validators.required]],
-      address: ["",[Validators.required]],
-      jobPositonID: [0,[Validators.required]],
-      salary: [0,[Validators.required]],
-      bonus: [0,[Validators.required]],
+      name: [data?.name || "", [Validators.required]],
+      lastname: [data?.lastname || "", [Validators.required]],
+      genderId: [data?.genderId ||  1,[Validators.required]],
+      maritalStatusId: [data?.maritalStatusId || 1 ,[Validators.required]],
+      birthdayDate: [data?.birthdayDate || new Date(),[Validators.required]],
+      age: [data?.age ||  0,[Validators.required]],
+      dpi: [data?.dpi ||  0,[Validators.required]],
+      nit: [data?.nit || 0],
+      socialSecurity: [data?.socialSecurity || 0],
+      irtra: [data?.irtra ||  0],
+      passport: [data?.passport ||  0,[Validators.required]],
+      address: [data?.address ||  "",[Validators.required]],
+      jobPositonID: [data?.jobPositonID ||  0,[Validators.required]],
+      salary: [data?.salary || 0,[Validators.required]],
+      bonus: [data?.bonus ||  0,[Validators.required]],
     });
   }
 
@@ -97,12 +100,8 @@ export class EmployeeComponent implements OnInit {
       const employee = this.employeeForm.value;
       this.api.post("employee", employee).subscribe((res: any) => {
         this.getEmployes();
-        Swal.fire({
-           text: 'Empleado registrado correctamente',
-          icon: 'success',
-          confirmButtonText: 'Ok'
-        })
-        this.employeeForm.reset();
+        this.showAlert("Empleado registrado correctamente!!!");
+        this.cleanForm();
         
       }, (error: HttpErrorResponse) => {
         (error.status === 404) 
@@ -112,5 +111,70 @@ export class EmployeeComponent implements OnInit {
     }else{
       this.employeeForm.markAllAsTouched();
     }
+  }
+
+  editEmployee(){
+    if(this.employeeForm.valid){
+      const employee = this.employeeForm.value;
+      this.api.put(`employee/${this.employeeId}`, employee).subscribe((res: any) => {
+        this.getEmployes();
+        this.showAlert("Empleado actualizado correctamente!!!");
+        this.cleanForm();
+        
+      }, (error: HttpErrorResponse) => {
+        (error.status === 404) 
+          console.log("error")
+      });
+
+    }else{
+      this.employeeForm.markAllAsTouched();
+    }
+  }
+
+  employeeDelete(employeeID : number){
+    
+      this.api.delete(`employee/${employeeID}`).subscribe((res: any) => {
+        this.getEmployes();
+        this.showAlert("Empleado eliminado correctamente!!!");
+      }, (error: HttpErrorResponse) => {
+        (error.status === 404) 
+          console.log("error")
+      });
+  }
+
+  confirmDeleteEmployee(employeeID : number){
+    Swal.fire({
+      title: "Esta seguro?",
+      text: "Una vez eliminado, ¡no podrá recuperar a este empleado!",
+      icon: 'warning',
+      showConfirmButton: true,
+      showCancelButton: true     
+    })
+    .then((willDelete) => {
+
+        if(willDelete.value){
+          this.employeeDelete(employeeID);
+        }
+    });
+  }
+
+
+  employeeSelected(employee: Employee){
+    this.employeeId = employee.id;
+    this.employeeForm = this.createFormGroup(employee);
+    this.btnEdit = true;
+  }
+
+  showAlert(msg : string){
+    Swal.fire({
+      text: msg,
+     icon: 'success',
+     confirmButtonText: 'Ok'
+   })
+  }
+
+  cleanForm(){
+    this.employeeForm.reset();
+    this.employeeId = 0;
   }
 }
